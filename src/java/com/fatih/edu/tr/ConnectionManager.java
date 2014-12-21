0211cc297863560fd4611356fd0abc4b7c48ec1e
Ahmet ERKOC <aerkoc01@gmail.com>
2014-12-21 16:02:03 +0200

    Connection Manager Class Added

---
+++ src/java/com/fatih/edu/tr/ConnectionManager.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fatih.edu.tr;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author erkoc
+ */
+public class ConnectionManager {
+    
+    public static Connection getConnection(){
+        
+      Connection conn = null;
+            
+           try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost/filmProjesi", "root", "sly6465");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return conn;
+    }
+    
+}
--- web/newTask.jsp
+++ web/newTask.jsp
@@ -12,13 +12,16 @@
         <title>New Task Page</title>
     </head>
     <body>
+        <div id="divForm" name="form" style="border: 2px black solid;">
         <form method="post" action="newTaskServlet" enctype="multipart/form-data">
             <table>
                 <tr><td><b>Title:</b></td><td><input type="text" name="title"/></td></tr>
                 <tr><td><b>Description:</b></td><td><input type="text" name="description"/></td></tr>
                 <tr><td><b>Due-Date:</b></td><td><input type="datetime-local" name="due_date"/></td></tr>
                 <tr><tr></tr><td><input type="file"  name="file" value="Choose file"/></td></tr>
+            <tr><td></td><td><input type="submit" value="Create Task"/></td></tr>
             </table>
         </form>
+        </div>
     </body>
 </html>
